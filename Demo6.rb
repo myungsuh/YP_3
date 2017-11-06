1c13d6dd0b4d2d4b2480bf0260658e985b40a213
Myung Suh Son <myungsuhson@gmail.com>
2017-11-06 00:15:52 -0700

    Hello Universe!~

---
+++ Demo6.rb
@@ -0,0 +1,10 @@
+write_handler = File.new("yourSum.out", "w")
+#creates a new file
+write_handler.puts("Random Text").to_s
+#writes into the new file
+write_handler.close
+#closes the new file
+data_from_file = File.read("yourSum.out")
+#reads the new file
+puts "Data From File : " + data_from_file
+#writes out the text from the file
